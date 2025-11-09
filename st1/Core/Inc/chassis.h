6bad2782f749a37e794bbc56db82f8b86c6e3369
Waterkey3rd <huxinke7@163.com>
2025-11-09 21:57:24 +0800

    定义写完

--- st1/Core/Inc/chassis.h
+++ st1/Core/Inc/chassis.h
@@ -2,12 +2,31 @@
 #define CHASSIS_H
 
 #include "motor.h"
+#include "tim.h"
 #include <stdint.h>
 
 #define motor1_d 1//motor1_direction
 #define motor2_d 1//motor2_direction
 #define motor3_d 1//motor3_direction
 #define motor4_d 1//motor4_direction
+//start motor_in_channel
+#define motor1_in1_tim htim4
+#define motor1_in1_channel TIM_CHANNEL_1
+#define motor2_in1_tim htim4 
+#define motor2_in1_channel TIM_CHANNEL_2
+#define motor3_in1_tim htim4 
+#define motor3_in1_channel TIM_CHANNEL_3
+#define motor4_in1_tim htim4 
+#define motor4_in1_channel TIM_CHANNEL_4
+#define motor1_in2_tim htim3
+#define motor1_in2_channel TIM_CHANNEL_1
+#define motor2_in2_tim htim3 
+#define motor2_in2_channel TIM_CHANNEL_2
+#define motor3_in2_tim htim3 
+#define motor3_in2_channel TIM_CHANNEL_3
+#define motor4_in2_tim htim3 
+#define motor4_in2_channel TIM_CHANNEL_4
+//end motor_in_channel
 
 enum ChassisMotionState{//ChassisMotionState
     CHASSIS_STATE_STOPPED,
@@ -23,6 +42,6 @@ extern uint8_t chassis_angular_velocity;//chassis_angular_velocity,but use pwm_d
 extern enum ChassisMotionState current_chassis_state;//chassis_state,inform the chassis_state_machine 
 
 void chassis_init();//init,start all motors'pwm,set all motor's pwm_duty_cycle 0
-
+void chassis_motion();//arrcording to the variable "current_chassis_state" ,to set the chassis'movement.
 
 #endif /* CHASSIS_H */
--- st1/Core/Inc/motor.h
+++ st1/Core/Inc/motor.h
@@ -1,8 +1,17 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-void motor_init(void);
-void motor_set_speed(int speed);
-void motor_stop(void);
+#include "tim.h"
+#include <stdint.h>
+
+typedef enum{
+    MOTOR_DIR_FORWARD,
+    MOTOR_DIR_REVERSE,
+    MOTOR_DIR_STOP
+}Motor_Sate;
+
+void motor_init(TIM_HandleTypeDef *htim,uint32_t Channel);//init
+void motor_set_state(uint8_t speed,Motor_Sate state,TIM_HandleTypeDef *htim,uint32_t Channel);
+void motor_stop(TIM_HandleTypeDef *htim,uint32_t Channel);
 
 #endif /* MOTOR_H */
